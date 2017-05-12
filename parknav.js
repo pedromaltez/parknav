4166177b931559c808e51ab1e7258c57ad5f5a52
Pedro Maltez <pedro@maltez.org>
2017-05-12 12:28:21 +0100

    Add files via upload

--- parknav.js
+++ parknav.js
@@ -238,11 +238,11 @@ ITEM PAGE: http://codecanyon.net/item/jet-responsive-parknav-section/5719593
 
   // Stop menu links from working when using mobile accordion style menu
 
-  $(".parknav-section-name").click(function(e) {
-    e.preventDefault();
-    console.log("preventDefault", e)
-  })
 
 }(jQuery));
 
 
+$(".parknav-section-name").click(function(e) {
+  e.preventDefault();
+  console.log("preventDefault", e)
+})();
